b13b60966cc4e31bb772d6a63c27e92cca7e8e8c
Osmulak122 <34412964+Osmulak122@users.noreply.github.com>
2023-03-15 16:55:52 +0100

    Update footer

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -79,7 +79,7 @@ const Auth = () => {
 
 
   return (
-    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')]  bg-no-repeat bg-center bg-fixed bg-cover">
         <div className="bg-black w-full h-full lg:bg-opacity-50">
             <nav className="px-12 py-5">
                 <img src="/images/logo.png" alt="Logo" className="h-12" />
@@ -151,8 +151,8 @@ const Auth = () => {
                     </p>
                 </div>
             </div>
+            <Footer />
         </div>
-        <Footer />
     </div>
   )
 }
