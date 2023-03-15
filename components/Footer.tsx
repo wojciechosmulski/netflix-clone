6e95077dfbaa846853d5c58bfaf15959ac735763
Osmulak122 <34412964+Osmulak122@users.noreply.github.com>
2023-03-15 16:52:41 +0100

    Fix footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     
-<footer className="bottom-0 z-20 w-full p-4 flex items-center justify-center">
+<footer className="w-full sticky top-[100vh] p-4 flex justify-center bg-black/70">
     <span className="text-sm text-gray-400">Made with ♥ by Wojciech Osmulski 2023</span>
 </footer>
 
