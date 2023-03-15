105aa2e40905fdc41213e274bfc0d8ae65391aaa
Osmulak122 <34412964+Osmulak122@users.noreply.github.com>
2023-03-15 14:40:44 +0100

    fix movie card overlap

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -21,7 +21,7 @@ const MovieCard: FC<MovieCardProps> = ({ data }) => {
         className='cursor-pointer object-cover transition duration-100 shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-300 w-full h-[12vw]'
       />
       <div className='opacity-0 absolute top-0 transition duration-200 z-10 invisible sm:visible delay-300 w-full scale-0 group-hover:scale-110
-      group-hover:-translate-y-[6vw] group-hover:translate-x-[2vw] group-hover:opacity-100
+      group-hover:-translate-y-[6vw] group-hover:opacity-100
       '>
         <img 
         className='cursor-pointer object-cover transition duration shadow-xl rounded-t-md w-full h-[12vw]'
